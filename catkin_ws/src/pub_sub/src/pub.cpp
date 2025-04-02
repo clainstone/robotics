8942d7038a178f6e706e6c84dd23986619346de2
alessandro <alessandro.palliccia@mail.polimi.it>
2025-04-03 00:57:02 +0200

    added a new type of message

--- catkin_ws/src/pub_sub/src/pub.cpp
+++ catkin_ws/src/pub_sub/src/pub.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "pub_sub/Num.h"
 
 
 
@@ -13,13 +14,13 @@ int main(int argc, char **argv){
 	//the last argument is the size of the buffer : queue size
 
 	//"chatter" is the name of the topic
-	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1);
+	ros::Publisher chatter_pub = n.advertise<pub_sub::Num>("chatter", 1);
 
 	//loop _rate function creates a new object of type Rate
 	ros::Rate loop_rate(10);
 
 	while (ros::ok()){
-
+		if(false){
 	    	std_msgs::String msg;
 
                 msg.data = "hello world!";
@@ -33,9 +34,15 @@ int main(int argc, char **argv){
     		ros::spinOnce();
 
     		loop_rate.sleep();
-    	
+		}
+		else{
+			static int i=0;
+			i=(i+1)%1000;
+			pub_sub::Num msg;
+			msg.num =i;
   	}
 
 
   	return 0;
+}
 }
