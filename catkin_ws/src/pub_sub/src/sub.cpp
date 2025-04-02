920eca15bd2e45f90ebe654c045d59f82eb6d4f8
alessandro <alessandro.palliccia@mail.polimi.it>
2025-04-03 01:31:07 +0200

    .

--- catkin_ws/src/pub_sub/src/sub.cpp
+++ catkin_ws/src/pub_sub/src/sub.cpp
@@ -1,21 +1,28 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "pub_sub/Num.h"
 
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg){
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
+//ConstPtr è un alias per un puntatore costante a un'istanza del messaggio. È definito automaticamente da ROS per ogni tipo di messaggio.
+//Significa che il puntatore msg punta a un'istanza immutabile del messaggio ricevuto. 
+//Non puoi modificare il contenuto del messaggio all'interno della funzione di callback.
+void pubCallback(const pub_sub::Num::ConstPtr& msg){
+  ROS_INFO("I heard: [%d]", msg->num);
+}
 int main(int argc, char **argv){
   	//
 	ros::init(argc, argv, "listener");
 
 	ros::NodeHandle n;
-  	ros::Subscriber sub = n.subscribe("chatter", 1, chatterCallback);
+  	ros::Subscriber sub = n.subscribe("chatter", 1, pubCallback);
 
   	ros::spin();
 
   return 0;
 }
 
-
+<
